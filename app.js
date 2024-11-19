db7f3a2022eae6087be1dfa8401edd4a584d9e14
Sreymey <sreymey.nai@institute.pse.ngo>
2024-11-19 08:34:21 +0700

    my football system

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+// Middleware
+app.use(express.json());
+
+// Routes
+// const itemRoutes = require('./routes/itemRoutes');
+// const productRoutes = require('./routes/productRoutes')
+
+
+// app.use('/api/items', itemRoutes);
+// app.use('/api/products',productRoutes);
+
+module.exports = app;
---
+++ routes/productRoutes.js
@@ -0,0 +1,26 @@
+// const express = require('express');
+// const router = express.Router();
+// const Product = require('../models/product');
+
+// // GET all items
+// router.get('/', async (req, res) => {
+//     try {
+//         const products = await Product.find();
+//         res.json(products);
+//     } catch (err) {
+//         res.status(500).json({ message: 'Server Error' });
+//     }
+// });
+
+// // POST a new item
+// router.post('/', async (req, res) => {
+//     try {
+//         const newProduct = new Product(req.body);
+//         const savedProduct = await newProduct.save();
+//         res.status(201).json(savedProduct);
+//     } catch (err) {
+//         res.status(400).json({ message: 'Bad Request', error: err.message });
+//     }
+// });
+
+// module.exports = router;
